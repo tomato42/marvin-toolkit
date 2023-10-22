8321f4e27dc1f1a9e77b4f27f539465bbd6c7c90
Hubert Kario <hubert@kario.pl>
2023-10-22 15:17:14 +0200

    generate also PKCS#8 DER files

--- step1.sh
+++ step1.sh
@@ -17,7 +17,8 @@ for size in 1024 2048 4096; do
     fi
 
     echo "RSA $size bit private key in old OpenSSL format is in" $(x509Key $name)
-    echo "RSA $size bit private key in PKCS#8 format is in" $(x509Key --pkcs8 $name)
+    echo "RSA $size bit private key in PKCS#8 PEM format is in" $(x509Key --pkcs8 $name)
+    echo "RSA $size bit private key in PKCS#8 DER format is in" $(x509Key --der --pkcs8 $name)
     echo "RSA $size bit private key in PKCS#12 format is in" $(x509Key --with-cert --pkcs12 $name)
     echo "RSA $size bit self-signed certificate is in" $(x509Cert $name)
     echo
