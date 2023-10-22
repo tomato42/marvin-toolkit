e8c570baefc4f20f2595ddf6724b8f0be39467f5
Hubert Kario <hubert@kario.pl>
2023-10-22 15:23:15 +0200

    make sure that there are no repeated ciphertexts

--- step2-alt.sh
+++ step2-alt.sh
@@ -4,8 +4,8 @@ PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa1024/cert.pem -o rsa1024_repeat \
 --repeat 100000 --verbose \
 no_structure no_padding=48 signature_padding=0 \
-valid_repeated_byte_payload="125 0xff" \
-valid_repeated_byte_payload="125 0x01" \
+valid_repeated_byte_payload="118 0xff" \
+valid_repeated_byte_payload="118 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
 valid=0 valid=118
@@ -14,8 +14,8 @@ PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa2048/cert.pem -o rsa2048_repeat \
 --repeat 100000 --verbose \
 no_structure no_padding=48 signature_padding=0 \
-valid_repeated_byte_payload="253 0xff" \
-valid_repeated_byte_payload="253 0x01" \
+valid_repeated_byte_payload="246 0xff" \
+valid_repeated_byte_payload="246 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
 valid=0 valid=192 valid=246
@@ -24,8 +24,8 @@ PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa4096/cert.pem -o rsa4096_repeat \
 --repeat 100000 --verbose \
 no_structure no_padding=48 signature_padding=0 \
-valid_repeated_byte_payload="509 0xff" \
-valid_repeated_byte_payload="509 0x01" \
+valid_repeated_byte_payload="502 0xff" \
+valid_repeated_byte_payload="502 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
 valid=0 valid=192 valid=502
